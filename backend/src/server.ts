e6b7218523b636440b49b5d1e9a07091e614d95d
Léu Almeida <leo@webid.net.br>
2020-06-03 16:10:43 -0300

    feat(server): implementing router file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
+
 app.use(express.json());
 
-app.get("/users", (request, response) => {
-  response.json({ ok: true })
-});
+app.use(routes);
 
 app.listen(3333);
