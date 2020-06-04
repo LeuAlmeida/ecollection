90bb75ea210d028a91e6e022ab1477b3d03e10e3
Léu Almeida <leo@webid.net.br>
2020-06-04 14:30:03 -0300

    feat(APP_PORT): insert environment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,4 +11,6 @@ app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3333);
+app.listen(process.env.APP_PORT || 3333, () => {
+  console.log(`⚡ Server is running on ${process.env.APP_PORT || 3333} ⚡`)
+});
