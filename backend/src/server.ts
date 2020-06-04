02327c933e4e8ac40c628a6f3e513a34560d8d37
Léu Almeida <leo@webid.net.br>
2020-06-04 15:28:18 -0300

    feat(server): implementing cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 import express from "express";
+import cors from "cors";
 import path from "path";
+
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(process.env.APP_PORT || 3333, () => {
-  console.log(`⚡ Server is running on ${process.env.APP_PORT || 3333} ⚡`)
+  console.log(`⚡ Server is running on ${process.env.APP_PORT || 3333} ⚡`);
 });
