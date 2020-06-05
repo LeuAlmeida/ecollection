0f0eeaf45082ab2fb93b6eb5d0180be2097a2df5
Léu Almeida <leo@webid.net.br>
2020-06-05 20:50:08 -0300

    feat(App): using Home component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Home />
+  );
 }
 
 export default App;
