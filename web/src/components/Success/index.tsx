c68ec9b95a3279ee0e1d9642e68d1738f75c017a
Léu Almeida <leo@webid.net.br>
2020-06-06 00:15:54 -0300

    chore(Success): improving Success Message

--- web/src/components/Success/index.tsx
+++ web/src/components/Success/index.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { FiCheckCircle } from 'react-icons/fi'
+import React, { useEffect } from "react";
+import { FiCheckCircle } from "react-icons/fi";
+import { useHistory } from "react-router-dom";
 
-import './styles.css'
+import "./styles.css";
 
 const Success = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push("/");
+    }, 3000);
+  }, [history]);
+
   return (
-    <div id="page-success">
-      <main className="message">
-        <FiCheckCircle />
-        <span>Cadastro concluído!</span>
-      </main>
+    <div id="body-success">
+      <div id="page-success">
+        <main className="message">
+          <FiCheckCircle />
+          <span>Cadastro concluído!</span>
+        </main>
+      </div>
     </div>
   );
 };
