21c8ee22a478036314f07cabe0f8ad5be9b2386b
Léu Almeida <leo@webid.net.br>
2020-06-07 21:36:43 -0300

    feat(Home): implementing Home structure

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
-import { Image, View, StyleSheet } from "react-native";
+import { Feather as Icon } from '@expo/vector-icons'
+import { Image, View, ImageBackground, Text, StyleSheet } from "react-native";
+import { RectButton } from 'react-native-gesture-handler'
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require("../../assets/logo.png")} />
-    </View>
+    <ImageBackground
+      source={require("../../assets/home-background.png")}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}      
+    >
+      <View style={styles.main}>
+        <Image source={require("../../assets/logo.png")} />
+        <Text style={styles.title}>Your waste collection marketplace</Text>
+        <Text style={styles.description}>
+          We help people find collection points efficiently.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>
+            <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -13,6 +38,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+    backgroundColor: "#f0f0f5",
   },
 
   main: {
