067f0da8f6963dcd9372a328d70035682ef101f2
Léu Almeida <leo@webid.net.br>
2020-06-08 01:01:59 -0300

    feat(Home): implementing select field to display ufs and cities

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Feather as Icon } from "@expo/vector-icons";
 import { Image, View, ImageBackground, Text, StyleSheet } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import RNPickerSelect from "react-native-picker-select";
+import axios from "axios";
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
+interface SelectItem {
+  label: string;
+  value: string;
+}
 
 const Home = () => {
+  const [ufs, setUfs] = useState<SelectItem[]>([]);
+  const [cities, setCities] = useState<SelectItem[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setSelectedCity] = useState("0");
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "http://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => ({
+          label: uf.sigla,
+          value: uf.sigla,
+          key: uf.sigla,
+        }));
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get<IBGECityResponse[]>(
+        `http://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const citiesNames = response.data.map((uf) => ({
+          label: uf.nome,
+          value: uf.nome,
+          key: uf.nome,
+        }));
+
+        setCities(citiesNames);
+      });
+  }, [selectedUf]);
+
   const navigation = useNavigation();
 
   function handleNavigateToPoints() {
-    navigation.navigate("Points");
+    navigation.navigate("Points", { selectedUf, selectedCity });
+  }
+
+  function handleSelectedUf(value: string) {
+    setSelectedUf(value);
+  }
+
+  function handleSelectedCity(value: string) {
+    setSelectedCity(value);
   }
 
   return (
@@ -26,6 +87,26 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
+        <RNPickerSelect
+          style={select}
+          onValueChange={(value) => handleSelectedUf(value)}
+          placeholder={{ label: "Select a state", key: "State" }}
+          items={ufs}
+          Icon={() => <Icon name="chevron-down" size={24} color="gray" />}
+        />
+
+        <RNPickerSelect
+          style={cities.length <= 0 ? disabledSelect : select}
+          onValueChange={(value) => handleSelectedCity(value)}
+          placeholder={{ label: "Select a city", key: "City" }}
+          items={cities}
+          disabled={cities.length <= 0 ? true : false}
+          Icon={() =>
+            cities.length <= 0 ? null : (
+              <Icon name="chevron-down" size={24} color="gray" />
+            )
+          }
+        />
         <RectButton style={styles.button} onPress={handleNavigateToPoints}>
           <View style={styles.buttonIcon}>
             <Text>
@@ -69,17 +150,6 @@ const styles = StyleSheet.create({
 
   footer: {},
 
-  select: {},
-
-  input: {
-    height: 60,
-    backgroundColor: "#FFF",
-    borderRadius: 10,
-    marginBottom: 8,
-    paddingHorizontal: 24,
-    fontSize: 16,
-  },
-
   button: {
     backgroundColor: "#34CB79",
     height: 60,
@@ -107,5 +177,35 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+
+const select = {
+  viewContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 24,
+    marginBottom: 8,
+  },
+  inputAndroid: {
+    fontSize: 18,
+    height: 60,
+    color: "#333",
+  },
+};
+
+const disabledSelect = {
+  viewContainer: {
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    borderRadius: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 24,
+    marginBottom: 8,
+  },
+  inputAndroid: {
+    fontSize: 18,
+    height: 60,
+    color: "#333",
+  },
+};
 
 export default Home;
