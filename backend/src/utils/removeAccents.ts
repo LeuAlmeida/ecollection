3c4602161a38c9f63c5c1ff6b8e269175e12d2aa
Léu Almeida <leo@webid.net.br>
2020-06-08 13:56:10 -0300

    feat(utils): create function to remove accents, insert hyphen and transform toLowerCase

---
+++ backend/src/utils/removeAccents.ts
@@ -0,0 +1,15 @@
+export default function RemoveAccents(string: string | string[]) {
+  var accents =
+    "ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž";
+  var accentsOut =
+    "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+  string = (<string>string).split("");
+  var strLen = string.length;
+  var i, x;
+  for (i = 0; i < strLen; i++) {
+    if ((x = accents.indexOf(string[i])) != -1) {
+      string[i] = accentsOut[x];
+    }
+  }
+  return string.join("").replace(/\s/g, "-").toLowerCase();
+}
