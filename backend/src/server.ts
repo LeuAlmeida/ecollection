92730a92dc55123d371e2809f31a4eb705b3682c
Léu Almeida <leo@webid.net.br>
2020-06-08 14:58:55 -0300

    feat(server): implementing errors validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { errors } from "celebrate";
 
 import routes from "./routes";
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(process.env.APP_PORT || 3333, () => {
   console.log(`⚡ Server is running on ${process.env.APP_PORT || 3333} ⚡`);
